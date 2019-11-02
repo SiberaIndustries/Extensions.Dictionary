0af611a7bdd9e84b341aae91f2225d4b454ebb27
Tobias Sibera <discjoggy@gmx.de>
2019-11-02 20:08:04 +0100

    Revert using Utf8Json because of deserialisation error - Update NuGet packages

--- Extensions.Dictionary/DictionaryExtensions.cs
+++ Extensions.Dictionary/DictionaryExtensions.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
-using Utf8Json;
 
 namespace Extensions.Dictionary
 {
