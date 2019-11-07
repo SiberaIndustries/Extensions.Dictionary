3669c3f9409a9d693ea333318c2641f4e3cb5272
Tobias Sibera <discjoggy@gmx.de>
2019-11-08 00:35:52 +0100

    Add SetValue and GetMemberType MemberInfo extension method

--- Extensions.Dictionary/MemberInfoExtensions.cs
+++ Extensions.Dictionary/MemberInfoExtensions.cs
@@ -9,6 +9,34 @@ namespace Extensions.Dictionary
         public static object? GetValue(this MemberInfo memberInfo, object? instance, ISerializerResolver serializerResolver) =>
             serializerResolver.GetPropertyValue(memberInfo, instance);
 
+        public static void SetValue(this MemberInfo memberInfo, object? instance, object? value)
+        {
+            switch (memberInfo?.MemberType)
+            {
+                case MemberTypes.Property:
+                    ((PropertyInfo)memberInfo).SetValue(instance, value);
+                    break;
+                case MemberTypes.Field:
+                    ((FieldInfo)memberInfo).SetValue(instance, value);
+                    break;
+                case null:
+                    throw new ArgumentNullException(nameof(memberInfo));
+                default:
+                    throw new NotSupportedException($"{nameof(memberInfo.MemberType)} {memberInfo.MemberType} not supported");
+            }
+        }
+
+        public static Type GetMemberType(this MemberInfo memberInfo)
+        {
+            return memberInfo?.MemberType switch
+            {
+                MemberTypes.Property => ((PropertyInfo)memberInfo).PropertyType,
+                MemberTypes.Field => ((FieldInfo)memberInfo).FieldType,
+                null => throw new ArgumentNullException(nameof(memberInfo)),
+                _ => throw new NotSupportedException($"{nameof(memberInfo.MemberType)} {memberInfo.MemberType} not supported")
+            };
+        }
+
         public static string GetName(this MemberInfo memberInfo, ISerializerResolver serializerResolver) =>
             serializerResolver.GetPropertyName(memberInfo);
 
