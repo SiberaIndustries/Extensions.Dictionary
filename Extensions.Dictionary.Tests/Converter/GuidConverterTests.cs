7a18a74f988a12ef01ea72c5a917ee18715fea50
Tobias Sibera <Tobias.Sibera@live.de>
2022-11-19 02:18:16 +0100

    Add tests

--- Extensions.Dictionary.Tests/Converter/GuidConverterTests.cs
+++ Extensions.Dictionary.Tests/Converter/GuidConverterTests.cs
@@ -8,14 +8,43 @@ namespace Extensions.Dictionary.Tests.Converter
         private readonly ConverterSettings settings = new();
         private readonly GuidConverter converter = new();
 
-        [Fact]
-        public void ConvertToGuid_Success()
+        [Theory]
+        [InlineData(GuidValueHandling.D)]
+        [InlineData(GuidValueHandling.N)]
+        [InlineData(GuidValueHandling.B)]
+        [InlineData(GuidValueHandling.P)]
+        [InlineData(GuidValueHandling.X)]
+        public void ConvertToLowerGuid_Success(GuidValueHandling valueHandling)
         {
+            var format = valueHandling.ToString();
+            settings.GuidHandling = valueHandling;
+
             var guid = Guid.NewGuid();
             Assert.True(converter.CanConvert(guid.GetType()));
 
             var dict = converter.Convert(guid, settings);
-            Assert.Equal(guid.ToString(), dict);
+            Assert.Equal(guid.ToString(format), dict);
+
+            var result = converter.ConvertBack(dict, settings);
+            Assert.Equal(guid, result);
+        }
+
+        [Theory]
+        [InlineData(GuidValueHandling.UpperD)]
+        [InlineData(GuidValueHandling.UpperN)]
+        [InlineData(GuidValueHandling.UpperB)]
+        [InlineData(GuidValueHandling.UpperP)]
+        [InlineData(GuidValueHandling.UpperX)]
+        public void ConvertToUpperGuid_Success(GuidValueHandling valueHandling)
+        {
+            var format = valueHandling.ToString().Replace("Upper", "");
+            settings.GuidHandling = valueHandling;
+
+            var guid = Guid.NewGuid();
+            Assert.True(converter.CanConvert(guid.GetType()));
+
+            var dict = converter.Convert(guid, settings);
+            Assert.Equal(guid.ToString(format).ToUpper(), dict);
 
             var result = converter.ConvertBack(dict, settings);
             Assert.Equal(guid, result);
